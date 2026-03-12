d146620a86aab781c161fa045505bc93824c2307
vvlaads <vvlaads@gmail.com>
2026-03-12 22:38:10 +0300

    Cleared LogarithmFunctions

--- src/main/java/com/swt/lab/logarithm/LogarithmFunctions.java
+++ src/main/java/com/swt/lab/logarithm/LogarithmFunctions.java
@@ -1,7 +1,7 @@
 package com.swt.lab.logarithm;
 
 public class LogarithmFunctions {
-    private Ln ln;
+    private final Ln ln;
 
     public LogarithmFunctions(Ln ln) {
         this.ln = ln;
@@ -9,9 +9,5 @@ public class LogarithmFunctions {
 
     public double log(double x, double base) {
         return ln.calculate(x) / ln.calculate(base);
-    }
-
-    public double log5(double x) {
-        return log(x, 5);
     }
 }
--- src/test/java/com/swt/lab/logarithm/LogarithmFunctionsTest.java
+++ src/test/java/com/swt/lab/logarithm/LogarithmFunctionsTest.java
@@ -1,11 +1,5 @@
 package com.swt.lab.logarithm;
 
-import org.junit.jupiter.api.Test;
-
 public class LogarithmFunctionsTest {
 
-    @Test
-    public void testA() {
-
-    }
 }

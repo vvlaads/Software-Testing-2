4a05c32f2a5a4bb5757d0f89f51039261d5de3a2
vvlaads <vvlaads@gmail.com>
2026-03-30 09:02:20 +0300

    Update sin test: added test periodicity, changed order of options in common test

--- src/test/java/com/swt/lab/trigonometry/SinTest.java
+++ src/test/java/com/swt/lab/trigonometry/SinTest.java
@@ -10,17 +10,42 @@ public class SinTest {
 
     @ParameterizedTest
     @CsvSource({
-            "0,0",
-            "1.57079633,1",
-            "-1.57079633,-1",
-            "3.14159265,0",
             "-3.14159265,0",
-            "1.0471975512,0.866025403784",
-            "2.09439510239,0.866025403784",
-            "-1.0471975512,-0.866025403784",
             "-2.09439510239,-0.866025403784",
+            "-1.57079633,-1",
+            "-1.0471975512,-0.866025403784",
+            "0,0",
+            "1.0471975512,0.866025403784",
+            "1.57079633,1",
+            "2.09439510239,0.866025403784",
+            "3.14159265,0",
     })
     void testSin(double x, double expected) {
         assertEquals(expected, sin.calculate(x), 1e-8);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "0",
+            "0.5",
+            "1.0",
+            "2.5",
+            "-1.3",
+            "10.0"
+    })
+    void testPeriodicity(double x) {
+        double period = 2 * Math.PI;
+
+        assertEquals(
+                sin.calculate(x),
+                sin.calculate(x + period),
+                1e-8
+        );
+
+        assertEquals(
+                sin.calculate(x),
+                sin.calculate(x - period),
+                1e-8
+        );
     }
 }

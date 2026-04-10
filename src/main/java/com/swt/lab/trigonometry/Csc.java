4685d0cca23789c1fbcd0c465b818b0712f373e6
vvlaads <vvlaads@gmail.com>
2026-04-10 07:38:07 +0300

    Added level3 of integration tests, removed throwing exception in csc and sec

--- src/main/java/com/swt/lab/trigonometry/Csc.java
+++ src/main/java/com/swt/lab/trigonometry/Csc.java
@@ -1,7 +1,5 @@
 package com.swt.lab.trigonometry;
 
-import static java.lang.Math.PI;
-
 public class Csc {
     private final Sin sin;
 
@@ -10,10 +8,6 @@ public class Csc {
     }
 
     public double calculate(double x) {
-        double EPS = 1e-10;
-        if (x % PI < EPS) {
-            throw new IllegalArgumentException("Csc is not defined for the selected argument");
-        }
         return 1 / sin.calculate(x);
     }
 }
--- src/main/java/com/swt/lab/trigonometry/Sec.java
+++ src/main/java/com/swt/lab/trigonometry/Sec.java
@@ -1,7 +1,5 @@
 package com.swt.lab.trigonometry;
 
-import static java.lang.Math.PI;
-
 public class Sec {
     private final Cos cos;
 
@@ -10,10 +8,6 @@ public class Sec {
     }
 
     public double calculate(double x) {
-        double EPS = 1e-10;
-        if (x % (PI / 2) < EPS && x % PI > EPS) {
-            throw new IllegalArgumentException("Sec is not defined for the selected argument");
-        }
         return 1 / cos.calculate(x);
     }
 }
